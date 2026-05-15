85da37fd8caf2323535fa79c928ad20f51a85016
Bharti Sharma <bharti.s@binmile.com>
2026-05-15 17:57:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT ?? 8081;
+
+app.get('/', (req, res)=>{
+    return res.json({msg:'Hello from the server'})
+})
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})

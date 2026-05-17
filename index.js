3f79345014d9b1662bd58dcc645f89a62d8aed43
Bharti Sharma <bharti.s@binmile.com>
2026-05-17 14:53:21 +0530

    v4

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8081;
 
 app.get('/', (req, res)=>{
-    return res.json({msg:'Hello from the server v3'})
+    return res.json({msg:'Hello from the server v4'})
 })
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
